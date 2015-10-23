9626bebc846c3dd7cb816d5252d6f1901dd9f3ba
Vlatka <vlatka.fundak@gmail.com>
2015-10-23 14:40:44 +0200

    Title

--- ConsoleToDo/ConsoleToDo/Program.cs
+++ ConsoleToDo/ConsoleToDo/Program.cs
@@ -8,8 +8,19 @@ namespace ConsoleToDo
 {
     class Program
     {
+        public static void DimensionsOfTheConsole()
+        {
+            Console.SetWindowSize(200,40);
+        }
+
         static void Main(string[] args)
         {
+            string title = "Super ToDo Aplication";
+
+            DimensionsOfTheConsole();
+            UIPresenter.ShowTitle(title);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ ConsoleToDo/ConsoleToDo/UIPresenter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleToDo
+{
+    static class UIPresenter
+    {
+        static public void ShowTitle(string title)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+
+            for (int i = 0; i < 200; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (title.Length / 2)) + "}", title));
+
+            for (int i = 0; i < 200; i++)
+            {
+                Console.Write("*");
+            }
+        }
+    }
+}
